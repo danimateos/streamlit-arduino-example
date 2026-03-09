557bc04be7602e02ede38d8003c52658c2433dd9
Daniel Mateos <daniel@mateos.io>
2026-03-09 15:38:17 +0100

    Refactor to decouple state from input

--- barduino_receive_serial/barduino_receive_serial.ino
+++ barduino_receive_serial/barduino_receive_serial.ino
@@ -1,31 +1,48 @@
+const int ledPin = 48;
+const int buzzerPin = 46;
+
 String content = "";
+int ledState = LOW;
+unsigned long beepTime = 0;
 
 void setup() {
   Serial.begin(9600);
 
-  pinMode(48, OUTPUT);
-  pinMode(46, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  pinMode(buzzerPin, OUTPUT);
 }
 
 void loop() {
-  Serial.println("Waiting for message...");
-
-    content = Serial.readStringUntil('\n');
-
-    Serial.print("received: ");
-    Serial.println(content);
-
-
-  if (content == "ledon") {
-    digitalWrite(48, HIGH);
-  } else if (content == "ledoff") {
-    digitalWrite(48, LOW);
-  } else if (content == "beep") {
-    tone(46, 880, 200);
-  }
-
-  content = "";
-
-  delay(500);
+  readInput();
+  update();
 }
 
+void update() {
+  digitalWrite(ledPin, ledState);
+
+  if (millis() - beepTime < 200) {
+    tone(buzzerPin, 880);
+  } else {
+    noTone(buzzerPin);
+  }
+}
+
+
+void readInput() {
+  Serial.println("Waiting for message...");
+
+  content = Serial.readStringUntil('\n');
+
+  Serial.print("received: ");
+  Serial.println(content);
+
+  if (content == "ledon") {
+    ledState = HIGH;
+  } else if (content == "ledoff") {
+    ledState = LOW;
+  } 
+  
+  if (content == "beep") {
+    beepTime = millis();
+  }
+}
